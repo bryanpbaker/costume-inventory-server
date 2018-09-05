965e31a0b7ae2558429907fe5732e4f7cd2a9747
Bryan Baker <bryanpbaker@gmail.com>
2018-09-05 09:02:14 -0500

    added delete costume

--- routes/costumes.js
+++ routes/costumes.js
@@ -14,6 +14,7 @@ router.get('/', async (req, res) => {
 
     res.json({
       success: true,
+      count: costumes.length,
       costumes
     });
   } catch (error) {
@@ -46,7 +47,32 @@ router.post('/', async (req, res) => {
 });
 
 /**
- * @todo Figure out why it won't update successfully
+ * @route GET api/v1/costumes/:id
+ */
+router.get('/:id', async (req, res) => {
+  try {
+    const costume = await Costume.findById(req.params.id);
+
+    if (costume) {
+      res.send({
+        success: true,
+        costume
+      });
+    }
+
+    res.send({
+      success: false,
+      message: 'No costume was found with that ID'
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      error
+    });
+  }
+});
+
+/**
  * @route PUT api/v1/costumes/:id
  * @description update a Costume with the given id
  */
@@ -54,17 +80,45 @@ router.put('/:id', async (req, res) => {
   try {
     // find the costume
     const costume = await Costume.findById(req.params.id);
-    // update the costume
-    Object.keys(costume.toJSON()).forEach(prop => {
-      costume[prop] = req.body[prop] || costume[prop];
-    });
 
-    // save the costume
-    const savedCostume = await costume.save();
+    if (costume) {
+      // update the costume
+      Object.keys(costume.toJSON()).forEach(prop => {
+        costume[prop] = req.body[prop] || costume[prop];
+      });
+
+      // save the costume
+      const savedCostume = await costume.save();
+
+      res.send({
+        success: true,
+        costume: savedCostume
+      });
+    }
+
+    res.send({
+      success: false,
+      costume: 'No costume was found with that ID'
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      error
+    });
+  }
+});
+
+/**
+ * @route DELETE api/v1/costumes/:id
+ * @description delete a costume with a given id
+ */
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleted = await Costume.deleteOne({ _id: req.params.id });
 
     res.send({
       success: true,
-      costume: savedCostume
+      deleted
     });
   } catch (error) {
     res.send({
